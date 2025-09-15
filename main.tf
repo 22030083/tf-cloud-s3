3cf263dc2310fe827d600630efb6ce47d62332ca
manisha <thakaremaheshmanisha@gmail.com>
2025-09-15 16:07:50 +0530

    hey

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Simple S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "tf-cloud-s3-bucket-12345" # must be globally unique
+  acl    = "private"
+}
+
+# Output the bucket name
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
